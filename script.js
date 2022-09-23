2c5cc9c203a9ce359cde771ed7cc28a0c4310b84
jacobbeacom <jdbeacom97@comcast.net>
2022-09-22 19:55:04 -0500

    added a function to display weather data on the page

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ document.getElementById('getWeather').addEventListener('click', async (e) => {
     const location = document.getElementById('searchbar').value;
     const result = await getWeatherData(location);
     console.log(result);
+    populatePage(result);
 });
 
 async function getWeatherData(location) {
     try {
       const units = getUnits();
       let response = await fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=Aac94a66ecd92872f9a5d676a0ccc3f08&units=${units}`, {
+          `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ac94a66ecd92872f9a5d676a0ccc3f08&units=${units}`, {
           mode: 'cors'
       });
       console.log(response);
@@ -29,10 +30,29 @@ async function getWeatherData(location) {
 
   function getUnits() {
     const checkBox = document.getElementById('units');
-
-    if (checkBox.checked == 'true') {
+    console.log(checkBox.checked);
+    if (checkBox.checked == true) {
         return 'metric';
     } else {
         return 'imperial';
     }
 }
+
+  function populatePage(result) {
+    
+    function unitChar () {
+      let unit;
+      let unitType = getUnits();  
+      if (unitType == 'imperial') {
+        unit = 'F';
+      } else {
+        unit = 'C';
+      };
+      return unit;
+    };
+    
+    currentLocation.innerHTML = result.name;
+    currentTemperature.innerHTML = `${result.main.temp} ${unitChar()}°`;
+    currentConditions.innerHTML = result.weather[0].description;
+
+  }
